edce063ebf08109919ce221f6c7155469b4efbc7
UdjOnline <e_davydov@mail.ru>
2024-02-28 00:04:19 +0400

    без изменений

--- src/l31/wclass/Main3.java
+++ src/l31/wclass/Main3.java
@@ -1,5 +1,10 @@
 package l31.wclass;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 @FunctionalInterface // аннотация помогает гарантировать, что интерфейс всем теребованиям
 interface Calculator { // создаём интерфейс с одним абстрактым методом - operate
     int operate(int a, int b); // этот метод принимает два аргумента и возвращает результат
@@ -48,9 +53,19 @@ class Main3 {
         Calculator multiply = (a, b) -> a * b;
         Calculator divide = (a, b) -> a / b;
 
-        System.out.println("Сумма 2 и 2 равна " + add.operate(2, 2));
-        System.out.println("Разность 2 и 2 равна " + subtract.operate(2, 2));
-        System.out.println("Умножение 2 на 2 равно " + multiply.operate(2, 2));
-        System.out.println("Деление 2 на 2 равно  " + divide.operate(2,2));
+        System.out.println("Сумма 2 и 2 равна " + add.operate(2, 2) + ";");
+        System.out.println("Разность 2 и 2 равна " + subtract.operate(2, 2) + ";");
+        System.out.println("Умножение 2 на 2 равно " + multiply.operate(2, 2) + ";");
+        System.out.println("Деление 2 на 2 равно  " + divide.operate(2,2) + ".");
+
+        List<String> strings = Arrays.asList("one", "two", "three"); //пример использования л-в с потоками данных
+        System.out.println("__________________________________________");
+        System.out.println("Фильтрация строк, начинающихся с символа t:");
+        // создание потока данных из списка строк
+        strings.stream()
+                // фильтрация строк, начинающихся с "t"
+                .filter(s -> s.startsWith("t"))
+                // вывод отфильтрованных строк
+                .forEach(System.out::println);
     }
 }
